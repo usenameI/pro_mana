5091d07e86ae898f862951b4aa09191d0ffd9e00
luyidada <10086683+luyidada@user.noreply.gitee.com>
2024-11-23 21:32:09 +0800

    经纬度功能提交

--- lib/getLoaction/getLocation.dart
+++ lib/getLoaction/getLocation.dart
@@ -1,124 +1,125 @@
+import 'dart:async';
 
 import 'package:geolocator/geolocator.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 ///定位服务
-class getLocation{
-   // 检查权限
- static Future<bool> checkqx()async{
-  LocationPermission permission;
-  permission = await Geolocator.checkPermission();
-  if (permission == LocationPermission.denied) {
-    permission = await Geolocator.requestPermission();
+class getLocation {
+  // 检查权限
+  static Future<bool> checkqx() async {
+    LocationPermission permission;
+    permission = await Geolocator.checkPermission();
     if (permission == LocationPermission.denied) {
-      return false;
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return false;
+      }
     }
+    return true;
   }
-  return true;
-  }
+
   ///检查手机定位服务是否开启同时开启定位服务
   static Future<bool> checkLocationPermission() async {
     var sta = await Permission.location.status;
     if (sta.isGranted) {
       return true;
-    }else{
+    } else {
       var end = await Permission.location.request();
-      if(end.isGranted){
+      if (end.isGranted) {
         return true;
-      }else{
+      } else {
         return false;
       }
     }
-    
   }
 
   ///手动打开经纬度功能
- static Future openLocation()async{
-   var state = await Permission.location.request();
-   if(state==PermissionStatus.granted){
-    return true;
-   }
-   return false;
+  static Future openLocation() async {
+    var state = await Permission.location.request();
+    if (state == PermissionStatus.granted) {
+      return true;
+    }
+    return false;
   }
+
   //返回经纬度
- static Future<Position> determinePosition() async {
-  bool serviceEnabled;
-  LocationPermission permission;
-  serviceEnabled = await Geolocator.isLocationServiceEnabled();
-  if (!serviceEnabled) {
-    return Future.error('Location services are disabled.');
-  }
-  permission = await Geolocator.checkPermission();
-  if (permission == LocationPermission.denied) {
-    permission = await Geolocator.requestPermission();
+  static Future<Position> determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled.');
+    }
+    permission = await Geolocator.checkPermission();
     if (permission == LocationPermission.denied) {
-      return Future.error('Location permissions are denied');
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
     }
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    var config = AndroidSettings(
+        forceLocationManager: true, timeLimit: const Duration(seconds: 2));
+    var p;
+    try {
+      p = await Geolocator.getCurrentPosition(
+        locationSettings: config,
+      );
+    } catch (e) {
+      p = await Geolocator.getLastKnownPosition(
+        forceAndroidLocationManager: true,
+      );
+    }
+    return p;
   }
-  if (permission == LocationPermission.deniedForever) {
-    // Permissions are denied forever, handle appropriately. 
-    return Future.error(
-      'Location permissions are permanently denied, we cannot request permissions.');
-  } 
-  // When we reach here, permissions are granted and we can
-  // continue accessing the position of the device.
-  var config=AndroidSettings(forceLocationManager: true,timeLimit:const Duration(seconds: 2));
-  var p;
-  try{
-     p = await Geolocator.getCurrentPosition(
-    locationSettings: config,
-  );
-  }catch(e){
-    p = await Geolocator.getLastKnownPosition(forceAndroidLocationManager: true,);
-  }
-  return p;
-}
+
   // 返回经纬度
- static Future<Map<String,num?>> getCor(context,{
- Duration? setTime
- }) async {
-  // When we reach here, permissions are granted and we can
-  // continue accessing the position of the device.
-  var config=AndroidSettings(forceLocationManager: true,timeLimit:setTime );
-  var p;
-  try{
-     p =  await Geolocator.getCurrentPosition(
-    locationSettings: config,
-  );
-  }catch(e){
-    p = await Geolocator.getLastKnownPosition(forceAndroidLocationManager: true);
-    if(p==null){
-
-      return {
-      'latitude':null,
-      'longitude': null,
-      };
+  static Future<Map<String, num?>> getCor(context, {Duration? setTime}) async {
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    var config =
+        AndroidSettings(forceLocationManager: true, timeLimit: setTime);
+    var p;
+    try {
+      p = await Geolocator.getCurrentPosition(
+        locationSettings: config,
+      );
+    } catch (e) {
+      p = await Geolocator.getLastKnownPosition(
+          forceAndroidLocationManager: true);
+      if (p == null) {
+        return {
+          'latitude': null,
+          'longitude': null,
+        };
+      }
     }
-    
-  }
-return {
-      'latitude':p?.latitude,
+    return {
+      'latitude': p?.latitude,
       'longitude': p?.longitude,
-};
+    };
   }
-///计算经纬度的距离
-static double getRection({
-  required double startLatitude,
-  required double startLongitude,
-  required double endLatitude,
-  required double endLongitude,
-}){
-  double distance = Geolocator.distanceBetween(
-    startLatitude, 
-    startLongitude, 
-    endLatitude, 
-    endLongitude
-    );
-    return distance;
-}
 
-    ///米数显示
-   static String formatDistance(double meters) {
+  ///计算经纬度的距离
+  static double getRection({
+    required double startLatitude,
+    required double startLongitude,
+    required double endLatitude,
+    required double endLongitude,
+  }) {
+    double distance = Geolocator.distanceBetween(
+        startLatitude, startLongitude, endLatitude, endLongitude);
+    return distance;
+  }
+
+  ///米数显示
+  static String formatDistance(double meters) {
     if (meters >= 1000) {
       double kilometers = meters / 1000;
       return '${kilometers.toStringAsFixed(2)} 千米'; // 保留两位小数
@@ -127,5 +128,24 @@ static double getRection({
     }
   }
 
+  static Stream<Position>? positionStream;
+  static StreamSubscription? subscription;
 
+  ///移动获取经纬度
+  static moveGetLo(Function(Position) callback) {
+    var config = AndroidSettings(
+        distanceFilter: 10,
+        forceLocationManager: true,
+        timeLimit: const Duration(seconds: 2));
+    positionStream = Geolocator.getPositionStream(locationSettings: config);
+    subscription = positionStream!.listen((Position position) {
+      callback(position);
+    });
+  }
+
+  static stopMoveGetLo() {
+    if (subscription != null) {
+      subscription?.cancel();
+    }
+  }
 }
--- lib/secondTime/secondTime.dart
+++ lib/secondTime/secondTime.dart
@@ -1,57 +1,103 @@
 import 'dart:async';
 
 import 'package:flutter/cupertino.dart';
+import 'package:provider/provider.dart';
 import 'package:tdesign_flutter/tdesign_flutter.dart';
 
-class secondTime extends StatefulWidget {  
+class StopController {
+  int init = 0;
+  Stop? stop;
+  DateTime? startTime;
+  Timer? time;
+
+  void startT() {
+    // 记录开始时间
+    startTime = DateTime.now();
+    time = Timer.periodic(const Duration(seconds: 1), (timer) {
+      init++;
+      print('log__$init');
+      stop?.notifyListeners();
+    });
+  }
+
+  void stopT() {
+    if (time != null) {
+      time!.cancel();
+    }
+  }
+}
+
+class Stop extends ChangeNotifier {}
+
+class stopWatch extends StatelessWidget {
+  StopController stopController;
+  stopWatch({super.key, required this.stopController});
+  String formatDuration(int seconds) {
+    int hours = seconds ~/ 3600; // 计算小时
+    int minutes = (seconds % 3600) ~/ 60; // 计算分钟
+    int secs = seconds % 60; // 计算剩余秒数
+    return '${hours.toString().padLeft(2, '0')}:${minutes.toString().padLeft(2, '0')}:${secs.toString().padLeft(2, '0')}';
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return ChangeNotifierProvider(
+        create: (context) => Stop(),
+        child: Consumer<Stop>(
+          builder: (context, value, child) {
+            stopController.stop = value;
+            return TDText(formatDuration(stopController.init));
+          },
+        ));
+  }
+}
+
+class secondTime extends StatefulWidget {
   Function callBack;
   int setSecond;
-  secondTime({
-     this.setSecond=10,
-    required this.callBack
-  });
-  @override  
-  _MyAppState createState() => _MyAppState();  
-}  
-  
-class _MyAppState extends State<secondTime> {  
-   DateTime? startTime;    
+  secondTime({this.setSecond = 10, required this.callBack});
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<secondTime> {
+  DateTime? startTime;
   Timer? time;
-  int init=0;
-  void startLoading() {  
-    // 记录开始时间  
-    startTime = DateTime.now();  
-    time=Timer.periodic(const Duration(seconds: 1), (timer) {
-      
+  int init = 0;
+  void startLoading() {
+    // 记录开始时间
+    startTime = DateTime.now();
+    time = Timer.periodic(const Duration(seconds: 1), (timer) {
       init++;
-      if(init==widget.setSecond){
+      if (init == widget.setSecond) {
         widget.callBack(true);
       }
-      setState((){});
-     });
+      setState(() {});
+    });
   }
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     WidgetsBinding.instance.addPostFrameCallback((_) {
       startLoading();
-     });
+    });
   }
+
   @override
   void dispose() {
     // TODO: implement dispose
     super.dispose();
-    if(time!=null){
+    if (time != null) {
       time!.cancel();
     }
   }
-  
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return TDText('累计时间: ${init}秒');
-  }  
-  
-  
+  }
 }
